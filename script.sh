983f8bc4395f7d025ef71843a1de403accfd7cdd
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-17 20:14:34 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
 crave run --no-patch -- bash -c "
 # Remove existing local manifests and reinitialize the repo
-rm -rf .repo/local_manifests &&
+rm -rf .repo/local_manifests build &&
+
+# Initialize the repo with the new source
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs &&
 
 # Clone new local_manifests repository
 git clone -b main https://github.com/MaheshTechnicals/local_manifests.git .repo/local_manifests &&
 
-# Remove old device, vendor, kernel, and frameworks/native directories
+# Remove old directories
 rm -rf device/xiaomi/miatoll &&
 rm -rf vendor/xiaomi/miatoll &&
 rm -rf kernel/xiaomi/sm6250 &&
 rm -rf hardware/xiaomi &&
 rm -rf hardware/sony/timekeep &&
 rm -rf frameworks/native &&
-
-# Initialize the repo with the new source
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs &&
 
 # Resync the repo
 /opt/crave/resync.sh &&
