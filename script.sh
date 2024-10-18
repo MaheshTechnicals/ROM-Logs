d00502ac3cafe2511c205cacb7e7b65ca4f98f34
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-18 10:08:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -19,6 +19,17 @@ rm -rf frameworks/native &&
 # Resync the repo
 /opt/crave/resync.sh &&
 
+# Clone necessary repositories after resync
+git clone -b 15 https://github.com/clarencekopitiam/kernel_xiaomi_sm6250.git kernel/xiaomi/sm6250 --depth=1 &&
+git clone -b lineage-21.0 https://github.com/clarencekopitiam/vendor_xiaomi_miatoll.git vendor/xiaomi/miatoll --depth=1 &&
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi --depth=1 &&
+git clone -b lineage-21 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1 &&
+git clone https://github.com/clarencelol/vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera --depth=1 &&
+git clone -b alpha https://github.com/MaheshTechnicals/vendor_lineage-priv.git vendor/lineage-priv/keys --depth=1 &&
+
+# Clone the frameworks/native repository
+git clone -b alpha-14 https://github.com/MaheshTechnicals/frameworks_native-alpha.git frameworks/native --depth=1 &&
+
 # Setup the build environment
 . build/envsetup.sh &&
 lunch lineage_miatoll-userdebug &&
