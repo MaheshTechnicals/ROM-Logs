6ced273e01ac39f2bcc306c3e3e7b4f4e7f1c4e5
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-20 17:24:10 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,33 +7,8 @@ repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lf
 # Clone new local_manifests repository
 git clone -b main https://github.com/MaheshTechnicals/local_manifests1 .repo/local_manifests &&
 
-# Remove old directories
-rm -rf device/xiaomi/miatoll &&
-rm -rf vendor/xiaomi/miatoll &&
-rm -rf kernel/xiaomi/sm6250 &&
-rm -rf hardware/xiaomi &&
-rm -rf hardware/sony/timekeep &&
-rm -rf frameworks/native &&
-rm -rf /tmp/src/android/.repo/projects/device/xiaomi/miatoll.git &&
-rm -rf /tmp/src/android/.repo/projects/frameworks/native.git &&
-rm -rf /tmp/src/android/.repo/projects/hardware/sony/timekeep.git &&
-
-
 # Resync the repo
 /opt/crave/resync.sh &&
-
-# Clone necessary repositories after resync
-rm -rf hardware/xiaomi &&
-rm -rf hardware/sony/timekeep &&
-rm -rf frameworks/native &&
-git clone -b lineage-21 https://github.com/LineageOS/android_hardware_sony_timekeep.git hardware/sony/timekeep --depth=1 &&
-git clone -b lineage-21 https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi --depth=1 &&
-git clone -b alpha-14 https://github.com/MaheshTechnicals/frameworks_native-alpha.git frameworks/native --depth=1 &&
-
-
-
-# Clone the frameworks/native repository
-git clone -b alpha-14 https://github.com/MaheshTechnicals/frameworks_native-alpha.git frameworks/native --depth=1 &&
 
 # Setup the build environment
 . build/envsetup.sh &&
