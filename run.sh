2c24671e6b8710654128b2b911c57720bd2f59ea
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-23 22:34:06 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 crave run --no-patch -- bash -c "
 curl https://raw.githubusercontent.com/MaheshTechnicals/ROM-Logs/refs/heads/main/script.sh | bash
-"
+" &&
+crave pull out/target/product/*/*.zip &&
+crave pull out/target/product/*/*.img
