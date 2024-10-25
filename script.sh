86299582083d760b5fc78ea2dd627c975f1b78ed
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-25 13:57:06 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests build &&
 
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs &&
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs &&
 
 git clone -b evo https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
