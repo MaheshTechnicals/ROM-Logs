fb1feb30efe74f159d0f76ebc8b6e3a1cc9bc0b5
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-28 14:08:51 +0530

    Revert "Update script.sh"
    This reverts commit 1813ffc708a73a8cea3a57301410da3dbf5534dd.

--- script.sh
+++ script.sh
@@ -1,11 +1,15 @@
 rm -rf .repo/local_manifests build &&
 
-repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs &&
+repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs &&
 
-git clone -b afterlife https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+git clone -b orion https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
 /opt/crave/resync.sh &&
 
 source build/envsetup.sh &&
 
-goafterlife miatoll
+lunch orion_miatoll-ap2a-userdebug &&
+
+make installclean &&
+
+mka space
