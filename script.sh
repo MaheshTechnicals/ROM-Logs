1813ffc708a73a8cea3a57301410da3dbf5534dd
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-28 14:08:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,11 @@
 rm -rf .repo/local_manifests build &&
 
-repo init -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs &&
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs &&
 
-git clone -b orion https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+git clone -b afterlife https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
 /opt/crave/resync.sh &&
 
 source build/envsetup.sh &&
 
-lunch orion_miatoll-ap2a-userdebug &&
-
-make installclean &&
-
-mka space
+goafterlife miatoll
