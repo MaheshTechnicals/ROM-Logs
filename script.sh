10a6b22e7df6f1c3b57448d50f9fd007c00a5e41
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-11-18 10:59:38 +0530

    Alphadroid

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 rm -rf .repo/local_manifests build &&
 
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs &&
+repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs &&
 
-git clone -b evo https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+git clone -b miatoll https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
 /opt/crave/resync.sh &&
 
 source build/envsetup.sh &&
 
-lunch lineage_miatoll-user &&
+lunch lineage_miatoll-userdebug &&
 
 make installclean &&
 
-m evolution
+brunch miatoll
