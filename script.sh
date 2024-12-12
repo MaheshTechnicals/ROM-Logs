ab2987e5773bcd24b9bf6797faadecf6035cff24
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-12-12 12:28:23 +0530

    Update script.sh : AfterLife

--- script.sh
+++ script.sh
@@ -1,15 +1,13 @@
 rm -rf .repo/local_manifests build &&
 
-repo init -u https://github.com/alphadroid-project/manifest -b alpha-14 --git-lfs &&
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs &&
 
-git clone -b miatoll https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+git clone -b afterlife https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
 /opt/crave/resync.sh &&
 
 source build/envsetup.sh &&
 
-lunch lineage_miatoll-userdebug &&
-
 make installclean &&
 
-brunch miatoll
+goafterlife miatoll
